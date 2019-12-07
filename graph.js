38405a212f7b47f784fc749cb029c07e203dc09a
Donald Tang <turazinyc@gmail.com>
2019-12-07 18:23:47 -0500

    Added hover actions

--- graph.js
+++ graph.js
@@ -1,46 +1,71 @@
-// leaves room for things like axes and legends
-const margin = { top: 40, right: 20, bottom: 50, left: 100 };
-const graphWidth = 560 - margin.left - margin.right;
-const graphHeight = 400 - margin.top - margin.bottom;
+const margin = { top: 40, right: 20, bottom: 50, left: 100 }; // leaves room for things like axes and legends
+const graphWidth = 560 - margin.left - margin.right; // graph width
+const graphHeight = 400 - margin.top - margin.bottom; // graph height
 
+/* CREATE SVG ELEMENT */
 const svg = d3
   .select(".canvas")
   .append("svg")
   .attr("width", graphWidth + margin.left + margin.right)
   .attr("height", graphHeight + margin.top + margin.bottom);
 
+/* CREATE GRAPH ELEMENT */
 const graph = svg
   .append("g")
   .attr("width", graphWidth)
   .attr("height", graphHeight)
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// scales
+/* SCALES */
+// x scale will be a time scale, we pass the date into it to determine horizontal positioning of points
 const x = d3.scaleTime().range([0, graphWidth]);
+// y scale will be a linear scale determined on distance
 const y = d3.scaleLinear().range([graphHeight, 0]);
 
-// axes group
+/* AXES GROUP */
+// append x axis group to graph
 const xAxisGroup = graph
   .append("g")
   .attr("class", "x-axis")
   .attr("transform", `translate(0, ${graphHeight})`);
 
+// append y axis group to graph
 const yAxisGroup = graph.append("g").attr("class", "y-axis");
 
-// d3 line path generator
+/* DOTTED LINES */
+
+// D3 LINE GENERATOR
 const line = d3
   .line()
   .x(function(d) {
-    return x(new Date(d.date));
+    return x(new Date(d.date)); // horizontal line will be as long as the output of time scale from date
   })
   .y(function(d) {
-    return y(d.distance);
+    return y(d.distance); // vertical line will be as high as output of linear scale from distance
   });
 
-// create line path element
+//  DOTTED LINE GROUP - will contain both x and y lines
+const dottedLines = graph
+  .append("g")
+  .attr("class", "lines")
+  .style("opacity", 0);
+
+const xDottedLine = dottedLines
+  .append("line")
+  .attr("stroke", "#aaa")
+  .attr("stroke-width", 1)
+  .attr("stroke-dasharray", 4);
+
+const yDottedLine = dottedLines
+  .append("line")
+  .attr("stroke", "#aaa")
+  .attr("stroke-width", 1)
+  .attr("stroke-dasharray", 4);
+
+/* CREATE PATH ELEMENT TO APPEND DOTTED LINES TO */
 const path = graph.append("path");
 
-// tooltip
+/* TOOLTIP */
 const tip = d3
   .tip()
   .attr("class", "tip card")
@@ -51,8 +76,9 @@ const tip = d3
     return content;
   });
 
-graph.call(tip);
+graph.call(tip); // call the toptip from graph
 
+/* UPDATE FUNCTION */
 const update = data => {
   // filter data based on activity. We want objects that are equal to selected activity
   data = data.filter(item => item.activity === activity);
@@ -62,14 +88,14 @@ const update = data => {
 
   // set scale domains
   // look at all of the data and compare all of the dates
-  x.domain(d3.extent(data, d => new Date(d.date)));
-  y.domain([0, d3.max(data, d => d.distance)]);
+  x.domain(d3.extent(data, d => new Date(d.date))); // x uses time scale for dates
+  y.domain([0, d3.max(data, d => d.distance)]); // y uses linear scale for distances
 
   // update path data - we must pass entire data array when passing data to a path element
   path
     .data([data])
     .attr("fill", "none")
-    .attr("stroke", "#00bfa5")
+    .attr("stroke", "#ffb74d")
     .attr("stroke-width", 2)
     .attr("d", line);
 
@@ -79,7 +105,7 @@ const update = data => {
   // remove unwanted points
   circles.exit().remove();
 
-  // update current pointers
+  // update circles already in graph
   circles.attr("cx", d => x(new Date(d.date))).attr("cy", d => y(d.distance));
 
   // add new points
@@ -159,6 +185,23 @@ const handleMouseOver = (d, i, n) => {
     .duration(100)
     .attr("r", 10)
     .attr("fill", "#fff");
+
+  // set x dotted cords
+  xDottedLine
+    .attr("x1", x(new Date(d.date))) // horizontal positioning of dotted lines will be based on x scale
+    .attr("x2", x(new Date(d.date)))
+    .attr("y1", graphHeight) // vertical position
+    .attr("y2", y(d.distance));
+
+  // set y dotted lines
+  yDottedLine
+    .attr("x1", 0)
+    .attr("x2", x(new Date(d.date)))
+    .attr("y1", y(d.distance))
+    .attr("y2", y(d.distance));
+
+  // show dotted lines
+  dottedLines.style("opacity", 1);
 };
 
 const handleMouseLeave = (d, i, n) => {
@@ -167,4 +210,7 @@ const handleMouseLeave = (d, i, n) => {
     .duration(100)
     .attr("r", 5)
     .attr("fill", "#ccc");
+
+  // hide dotted lines
+  dottedLines.style("opacity", 0);
 };
